812b56ef7c4d71b6b3c98087e0e549d0a71dc52a
ZEDIXUS_100K <joxaj045@gmail.com>
2025-10-30 18:15:29 +0500

    Create magic.py

---
+++ magic.py
@@ -0,0 +1,19 @@
+#1
+#ism = "javohir"
+#ismlar = ["Mominjon", "Bekmurod", "Abdullajon", "Diyor", ]
+#print = (f"{ismlar [0]} ishlar qalay ")
+
+#2
+#sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy,)
+#sonlar = [1,90,-20,534,999]
+#print(sonlar)
+
+#3
+#Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring
+#sonlar = [1,90,-20,534,999]
+#print(sonlar[0] + sonlar [4])
+
+#4
+#t_shaxslarva z_shaxslar degan 2 ta ro'yxat yarating va biriga o'zingiz eng ko'p hurmat qilgan tarixiy shaxslarn
+t_shaxslar = ["Amir Temur", ""]
+
