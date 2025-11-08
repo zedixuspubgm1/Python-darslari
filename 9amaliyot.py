20fa58e2018376f05939576ea4edbcb7e312ba69
Javohir Baxtiyarov <joxaj045@gmail.com>
2025-11-08 17:47:42 +0500

    Add files via upload

---
+++ 9amaliyot.py
@@ -0,0 +1,34 @@
+#1
+#Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
+#ismlar = ["Javohir","Diyor", "Isroil","Bekmurod","Abdullajon"]
+#for ism in ismlar:
+ #print(f"Salom, {ism}. hush kelibsiz")
+
+ #2
+ #Yuoqirdagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring (n o'rniga kod necha marta takrorlanganini yozing)
+ #print(f"ro'yxat {len(ismlar)} marta takrorlanadi")
+
+ #3
+ #10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
+#sonlar = list(range(11,100,2))
+#for son in sonlar:
+#   print(son**3)
+
+#4
+#Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang, va kinolar degan ro'yxatga saqlab oling. Natijani konsolga chiqaring.
+#kinolar = []
+#print("5ta sevimli kinoingizni qaysilar")
+#for k in range(5):
+ # kinolar.append(input(f"{k+1}-kino:"))
+#  print(kinolar)
+
+
+
+#5
+#Foydalanuvchidan bugun nechta odam bilan uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
+#insonlar = int(input("Bugun necha kishi bn suhbat qildingiz?>>>"))
+#ismlar = []
+#for inson in range(insonlar):
+#    ismlar.append(input(f"{inson+1}-suhbat qilgan odamingiz kim edi: "))
+#print(ismlar)
+
